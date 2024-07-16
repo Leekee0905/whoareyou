57e80872bfc21ee2ec1b8b47802e650e0e65e2f1
GiSeong Lee <cj8928@gmail.com>
2024-07-16 12:17:14 +0900

    update firebase 초기 세팅

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyWg0bbKGCU47h4knahtz5KXmtYBrWENs",
+  authDomain: "iiiii-76d96.firebaseapp.com",
+  projectId: "iiiii-76d96",
+  storageBucket: "iiiii-76d96.appspot.com",
+  messagingSenderId: "301834002586",
+  appId: "1:301834002586:web:517d4a2f905220b1a0ff23",
+  measurementId: "G-KCDYCN38NP",
+};
+
+export const app = initializeApp(firebaseConfig);
