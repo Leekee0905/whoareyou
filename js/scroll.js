2d7ded77272bab1d2a9fb43379212431bcb9aeaa
GiSeong Lee <cj8928@gmail.com>
2024-07-17 17:24:20 +0900

    fixed: prettier

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
-'use script';
+"use script";
 
 function goToScroll(id) {
-    let location = document.querySelector(id).offsetTop;
-    window.scrollTo({ top: location - 0, behavior: "smooth" });
-    }
+  let location = document.querySelector(id).offsetTop;
+  window.scrollTo({ top: location - 0, behavior: "smooth" });
+}
