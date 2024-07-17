161cef0b02f2c701662aa4efe8743c5527a28851
GiSeong Lee <cj8928@gmail.com>
2024-07-17 12:50:26 +0900

    update: update blog,github links

--- js/member.js
+++ js/member.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyWg0bbKGCU47h4knahtz5KXmtYBrWENs",
@@ -8,15 +12,13 @@ const firebaseConfig = {
   storageBucket: "iiiii-76d96.appspot.com",
   messagingSenderId: "301834002586",
   appId: "1:301834002586:web:517d4a2f905220b1a0ff23",
-  measurementId: "G-KCDYCN38NP"
+  measurementId: "G-KCDYCN38NP",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
 let row = {};
-
 
 const data = await getDocs(collection(db, "IIIII"));
 data.forEach((e) => {
@@ -26,9 +28,9 @@ console.log(row);
 
 const memberContainers = document.querySelectorAll(".members-container");
 
-const memberHTML = row.members.map((member, index) => {
-  return (
-  `<div class="members-info">
+const memberHTML = row.members
+  .map((member, index) => {
+    return `<div class="members-info">
     <div class="info-area">
       <img id="me${index}" />
       <div class="info-container">
@@ -41,16 +43,15 @@ const memberHTML = row.members.map((member, index) => {
       </div>
     </div>
     <div class="members-otherinfo">
-      <span>깃허브링크</span>
-      <span>블로그링크</span>
+      <span><a href="${member.github}">깃허브링크</a></span>
+      <span><a href="${member.blog}">블로그링크</a></span>
     </div>
-  </div>`);
-}).join("");
+  </div>`;
+  })
+  .join("");
 
-memberContainers.forEach(container => {
+memberContainers.forEach((container) => {
   container.innerHTML = memberHTML;
 });
 
-
-      // <div>Member ${index + 1}: ${member.name}</div>
-
+// <div>Member ${index + 1}: ${member.name}</div>
