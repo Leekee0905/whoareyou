f66ff105d248616f75b09d65b05863acb2e2b713
GiSeong Lee <cj8928@gmail.com>
2024-07-17 19:25:51 +0900

    delete: 겹치는 firebase setting code 삭제 후 export 통합

--- js/comment.js
+++ js/comment.js
@@ -1,22 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import {
   getDocs,
   collection,
   addDoc,
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDyWg0bbKGCU47h4knahtz5KXmtYBrWENs",
-  authDomain: "iiiii-76d96.firebaseapp.com",
-  projectId: "iiiii-76d96",
-  storageBucket: "iiiii-76d96.appspot.com",
-  messagingSenderId: "301834002586",
-  appId: "1:301834002586:web:517d4a2f905220b1a0ff23",
-  measurementId: "G-KCDYCN38NP",
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import db from "./firebase.js";
 
 $("#postingbtn").click(async function () {
   const name = $("#comment-name").val();
