7d683c5f8c23dd21b90664f5302277dc2d81be78
hanamstarfield <qnfdmlto@naver.com>
2024-07-17 14:34:55 +0900

    add scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
 'use script';
 
 function goToScroll(name) {
-    let location = document.querySelector("." + name).offsetTop;
+    let location = document.querySelector("#" + name).offsetTop;
+    console.log(location);
     window.scrollTo({top: location - 0, behavior: 'smooth'}) };
