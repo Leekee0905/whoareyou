cf51b62e9b184cfc6170ed1ea3b08fbb81ced14e
GiSeong Lee <cj8928@gmail.com>
2024-07-18 10:57:06 +0900

    fixed: db 변경 및 수정 삭제 하나씩만 되던거 수정

--- js/comment.js
+++ js/comment.js
@@ -10,64 +10,58 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import db from "./firebase.js";
 
-const list = collection(db, "CRtest");
-
 // DB 가지고 오기
-let docs = await getDocs(list);
-console.log(docs);
+const docs = await getDocs(collection(db, "comments"));
 
 // 댓글 출력
-docs["docs"].forEach((doc, idx) => {
-  let id = doc.id;
-  let row = doc.data();
+docs.forEach((doc, idx) => {
+  const id = doc.id;
+  const row = doc.data();
+  const name = row["name"];
+  const comments = row["comment"];
 
-  let user = row["user"];
-  let comments = row["comments"];
-
-  let temp_html = `
+  const temp_html = `
   <li>
-    <strong>${user}</strong>
+    <strong>${name}</strong>
     <p>${comments}</p>
     <div class="btn-area">
-      <button id="update" class="btn" value="${id}">수정</button>
-      <button id="delete" class="btn" value="${id}">삭제</button>
+      <button class="update btn" value="${id}">수정</button>
+      <button class="delete btn" value="${id}">삭제</button>
     </div>
   </li>
   `;
-  // 아이디가 있어야 삭제가 된다.. 근데 왜 그 전꺼 먼저 누르면 삭제가 안될까
+
   $("#list").append(temp_html);
 });
 
-// 댓글 등록
 $("#submit").click(async function () {
-  let user = $("#user").val();
-  let comments = $("#comments").val();
+  let name = $("#name").val();
+  let comment = $("#comment").val();
 
   let doc = {
-    user: user,
-    comments: comments,
+    name: name,
+    comment: comment,
   };
-  if (user === "") {
+
+  if (name === "") {
     alert("이름 입력 필요");
-  } else if (comments === "") {
+  } else if (comment === "") {
     alert("댓글 입력 필요");
   } else {
-    await addDoc(list, doc);
+    await addDoc(collection(db, "comments"), doc);
     alert("저장 완료!");
     window.location.reload();
   }
 });
 
 // 댓글 수정
-$("#update").click(async function () {
+$(".update").click(async function () {
   let thisValue = $(this).val();
   let comments = prompt("수정할 내용");
-
   if (comments.length != 0) {
-    await updateDoc(doc(db, "CRtest", thisValue), {
-      comments: comments,
+    await updateDoc(doc(db, "comments", thisValue), {
+      comment: comments,
     });
-
     window.location.reload();
   } else {
     window.location.reload();
@@ -75,19 +69,13 @@ $("#update").click(async function () {
 });
 
 // 댓글 삭제
-$("#delete").click(async function () {
+$(".delete").click(async function () {
   let thisValue = $(this).val();
-  const isDel = confirm("삭제");
 
+  const isDel = confirm("삭제");
   if (isDel == true) {
-    await deleteDoc(doc(db, "CRtest", thisValue));
+    await deleteDoc(doc(db, "comments", thisValue));
     alert("삭제 완료");
   }
-
   window.location.reload();
-});
-
-$("#modifybtn").click(async function () {
-  const title = $(".card-title").text();
-  console.log(title);
 });
